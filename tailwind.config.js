8a91cd53fdd75706d46c17e6ef20811f93975d42
iamarunAR7 <112094406+iamarunAR7@users.noreply.github.com>
2023-07-28 19:55:03 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./login.html","./main.html"],
+  theme: {
+    
+    extend: {
+       
+    },
+  },
+  plugins: [],
+}
+
